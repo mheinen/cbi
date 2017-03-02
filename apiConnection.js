6306a4c1001916b9b3e28028493700554150d127
Matthias Heinen <heinen.m@web.de>
2017-03-02 11:39:23 +0100

    Add files via upload

--- apiConnection.js
+++ apiConnection.js
@@ -7,27 +7,14 @@ var request = require('request');
 //API URL
 var API_URL = 'https://cbiapi.herokuapp.com';
 //var API_URL = 'http://localhost:3000';
-exports.doRequest = function(body, callback) {
-    request.post(API_URL, {json: {name1: body.name1, name2: body.name2 }}, function (error, response, body) {
+exports.doGreetRequest = function(body, callback) {
+
+    request.post(API_URL, {json: body}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
            // console.log(response);
             console.log('Returned from API: '+JSON.stringify(body));
             return callback(body);
         }
     });
-
-
-    /*    var request = new http.ClientRequest({
-     hostname: "127.0.0.1",
-     port: 8080,
-     // Path to the channel for the device we like to control (see REST documentation)
-     path: "/rest/items/minecraft_minecraft_switch_192_168_0_17__9998_88ad955d_ce14_4c78_bb2c_942a64a9da7c_channelPowered",
-     method: "POST",
-     headers: {
-     "Content-Type": "text/plain",
-     "Content-Length": Buffer.byteLength(body)
-     }
-     });
-     request.end(body)*/
 };
 
