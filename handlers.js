122bc2b01afd25ebc49e3b0939464c2bf705728e
Matthias Heinen <heinen.m@web.de>
2017-03-02 11:28:28 +0100

    Add files via upload

--- handlers.js
+++ handlers.js
@@ -9,12 +9,24 @@ var handlers = {
         var name1 = this.event.request.intent.slots.vorname_one.value;
         var name2 = this.event.request.intent.slots.vorname_two.value;
         var handle = this;
-        var payload = {name1: name1, name2: name2};
+        var payload = { intent: 'Greeting', name1: name1, name2: name2 };
         apiConnection.doRequest(payload, function(result) {
                 console.log('Back in Handler');
                 console.log(result);
                 handle.emit(':tell', 'Hallo ' + result.nameEins +' und ' + result.nameZwei + '!');
             });
+    },
+    'Select': function () {
+        console.log("Event: " + JSON.stringify(this.event));
+        console.log("Slots: " + JSON.stringify(this.event.request.intent.slots));
+        var tablename = this.event.request.intent.slots.tablename.value;
+        var handle = this;
+        var payload = { intent: 'Select', tablename: tablename };
+        apiConnection.doRequest(payload, function(result) {
+            console.log('Back in Handler');
+            console.log(result);
+            handle.emit(':tell', 'Ich habe ' + result.counter + ' ' + tablename + ' gefunden!');
+        });
     }
 };
 
